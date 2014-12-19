bcd2218a4fe1531080fd2fdb81e96b0c7d8909a6
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-12-18 23:56:47 -0800

    Allow for the passing of mimetypes

--- image-to-data-uri.js
+++ image-to-data-uri.js
@@ -1,8 +1,15 @@
 // converts a URL of an image into a dataURI
-module.exports = function (url, cb) {
+module.exports = function (url, mimeType, cb) {
     // Create an empty canvas and image elements
     var canvas = document.createElement('canvas'),
         img = document.createElement('img');
+
+    if ( typeof mimeType === 'function' ) {
+        cb = mimeType;
+        mimeType = null;
+    }
+
+    mimeType = mimeType || 'image/png';
 
     // allow for cross origin that has correct headers
     img.crossOrigin = "Anonymous"; 
@@ -17,7 +24,7 @@ module.exports = function (url, cb) {
         ctx.drawImage(img, 0, 0);
 
         // Get the data-URI formatted image
-        cb( null, canvas.toDataURL('image/png') );
+        cb( null, canvas.toDataURL( mimeType ) );
     };
 
     img.onerror = function () {
